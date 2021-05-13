e90bb7a9b17b6f02a6656824939d024d0f1f977d
Aleksandr <drammtv@gmail.com>
2021-05-13 09:34:36 +0700

    1.2 Tracking objects

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from yolov5.utils.datasets import letterbox
 from yolov5.utils.general import non_max_suppression
 from yolov5.utils.torch_utils import select_device
 
+from sort.sort import *
 
 
 class Detect():
@@ -19,7 +20,7 @@ class Detect():
         self.img_size = img_size
         self.conf = conf
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=weights)  # custom model
-
+        self.mot_tracker = Sort(max_age=5, iou_threshold=0.1, min_hits=2)
 
     def detect_frame(self, img0s):
         device = select_device('cpu')
@@ -43,8 +44,6 @@ class Detect():
         return self.draw_bbox(img0s, pred, ['bicycle', 'car', 'motorcycle', 'bus', 'car'], w, h)
 
 
-
-
     def draw_bbox(self, image, bboxes, classes, w, h, show_label=True):
         num_classes = len(classes)
         image_h, image_w, _ = image.shape
@@ -57,24 +56,40 @@ class Detect():
         random.seed(None)
         h = image_h / h
         w = image_w / w
+
+        rect_and_class = []
+
+        fontScale = 0.5
+        bbox_thick = int(0.4 * (image_h + image_w) / self.img_size)
+
         if bboxes is not None:
             for i, bbox in enumerate(bboxes):
                 coor = np.array(bbox[:4], dtype=np.int32)
-                fontScale = 0.5
+
                 score = bbox[4]
                 class_ind = int(bbox[5])
                 bbox_color = colors[class_ind]
-                bbox_thick = int(0.4 * (image_h + image_w) / self.img_size)
+
                 c1, c2 = (int(coor[0] * w), int(coor[1] * h)), (int(coor[2] * w), int(coor[3] * h))
-                cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
 
-                if show_label:
-                    bbox_mess = '%s: %.2f' % (classes[class_ind], score)
-                    t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick // 2)[0]
-                    cv2.rectangle(image, c1, (c1[0] + t_size[0], c1[1] - t_size[1] - 3), bbox_color, -1)
+                # cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
 
-                    cv2.putText(image, bbox_mess, (c1[0], c1[1] - 2), cv2.FONT_HERSHEY_SIMPLEX,
-                                fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
+                rect_and_class.append([c1[0], c1[1], c2[0], c2[1], class_ind])
+
+            nd = np.array(rect_and_class)
+            track_bbs_ids = self.mot_tracker.update(nd)
+
+            for obj in track_bbs_ids:
+                x1, y1, x2, y2 = obj[:4].astype(int)
+                _id = obj[4].astype(int)
+                bbox_mess = '%d' % (_id)
+                # t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick // 2)[0]
+                cv2.rectangle(image, (x1, y1), (x2, y2), color=colors[0])
+
+                cv2.putText(image, bbox_mess, (x1, y1 - 2), cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
+            print([i for i in track_bbs_ids[:,4]])
+
 
         return image
 
@@ -93,7 +108,7 @@ if __name__ == '__main__':
         if time_elapsed > 1. / 60:
             prev = time.time()
             frame = detect.detect_frame(frame)
-            fps_str = 'FPS {0:.2}'.format(1 / (time.time()-prev))
+            fps_str = 'FPS {0:.2}'.format(1 / (time.time() - prev))
             cv2.putText(frame, fps_str, (4, 24), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, lineType=cv2.LINE_4)
             cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
